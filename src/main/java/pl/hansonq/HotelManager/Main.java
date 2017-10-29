fbd31a63801efbd9ba1bb4edad4a0bc04a3682cb
hansonqC <lcacbcac@gmail.com>
2017-10-29 11:45:06 +0100

    Ini

--- src/main/java/pl/hansonq/HotelManager/Main.java
+++ src/main/java/pl/hansonq/HotelManager/Main.java
@@ -3,9 +3,7 @@ package pl.hansonq.HotelManager;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-/**
- * Created by lukasz on 2017-10-28.
- */
+
 public class Main extends Application{
     public void start(Stage primaryStage) throws Exception {
 
